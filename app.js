4d41902f2ca8283f44e94ed186459b7c658368a9
Timo Verbrugghe <timo@hotmail.be>
2023-09-28 14:19:01 +0200

    Disable socket mode

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const openaiApiKey = process.env.OPENAI_API_KEY;
 // Initializes your app with your bot token and app token
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
+  // socketMode: true,
+  // appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   port: process.env.PORT || 3000
 });
